a7e8678ead9e0721b8bb0bea076b43c8cc7eb93b
knyazevi81 <73337376+knyazevi81@users.noreply.github.com>
2023-03-04 04:54:03 +0300

    fix: calculate_day_by_week

--- get_timetable.py
+++ get_timetable.py
@@ -4,6 +4,7 @@
 # и внесения в базу данных
 
 from requests import get
+import datetime
 from tokens import groupapi
 from tokens import currentweekapi
 
@@ -13,27 +14,30 @@ data: dict = read.json()
 now_week: int = last_week.json()['week']
 
 
+def calculate_day_by_week(now_day: int, now_week: int) -> datetime.time:
+    """Функция определения даты."""
+    start_date = datetime.date(2023, 2, 6)
+    delta = datetime.timedelta(days=7 * (now_week - 1) + now_day - 1)
+    return start_date + delta
+
+
 def parse_api(now_day: int, now_week: int) -> str:
     """Перебор пар по типо дня недели."""
     day: str = str(now_day)
+    print(calculate_day_by_week(now_day, now_week))
     try:
         for i in range(len(data['schedule'][day]['lessons'])):
             for j in range(len(data['schedule'][day]['lessons'][i])):
                 if now_week in data['schedule'][day]['lessons'][i][j]['weeks']:
                     print(data['schedule'][day]['lessons'][i][j]['name'],
                           data['schedule'][day]['lessons'][i][j]['types'],
+                          data['schedule'][day]['lessons'][i][j]['rooms'][0],
                           data['schedule'][day]['lessons'][i][j]['time_start'],
                           '-',
                           data['schedule'][day]['lessons'][i][j]['time_end'])
     except KeyError:
-        print('выходные')
-
-
-def date_lessons(now_day: int, now_week: int):
-    """Функция определения даты."""
-    day: str = str(now_day)
-    print(data['schedule'][day]['lessons'])
+        print('Выходные')
 
 
 if __name__ == '__main__':
-    parse_api(4, 1)
+    parse_api(1, now_week)
