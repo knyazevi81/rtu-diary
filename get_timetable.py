abfb0ae47892b99fcebcdb55004b2dcf1fd17db2
knyazevi81 <73337376+knyazevi81@users.noreply.github.com>
2023-03-04 03:00:01 +0300

    Update get_timetable.py
    adding an activity parser by parameters such as the day of the week and the week

--- get_timetable.py
+++ get_timetable.py
@@ -1,26 +1,39 @@
+# coder: knyazevi81
+# project: rtu-diary
+# Модуль проекта по определению предетов по заданным параметрам
+# и внесения в базу данных
+
 from requests import get
+from tokens import groupapi
+from tokens import currentweekapi
 
-read = get('https://schedule.mirea.ninja/api/schedule/%'
-           'D0%91%D0%91%D0%91%D0%9E-04-21/full_schedule')
-last_week = get('https://schedule.mirea.ninja/api/schedule/current_week')
+read = get(groupapi)
+last_week = get(currentweekapi)
+data: dict = read.json()
+now_week: int = last_week.json()['week']
 
 
-def get_week_lessons(date: int):
-    data: dict = read.json()
-    day_of_week = {
-        '1': 'понедельник',
-        '2': 'вторник',
-        '3': 'среда',
-        '4': 'четверг',
-        '5': 'пятница',
-        '6': 'суббота',
-        '7': 'восскресенье'
-    }
+def parse_api(now_day: int, now_week: int) -> str:
+    """Перебор пар по типо дня недели."""
+    day: str = str(now_day)
+    try:
+        for i in range(len(data['schedule'][day]['lessons'])):
+            for j in range(len(data['schedule'][day]['lessons'][i])):
+                if now_week in data['schedule'][day]['lessons'][i][j]['weeks']:
+                    print(data['schedule'][day]['lessons'][i][j]['name'],
+                          data['schedule'][day]['lessons'][i][j]['types'],
+                          data['schedule'][day]['lessons'][i][j]['time_start'],
+                          '-',
+                          data['schedule'][day]['lessons'][i][j]['time_end'])
+    except KeyError:
+        print('выходные')
 
-    print(day_of_week[str(1)])
-    print(data['schedule'][str(1)]['lessons'][1][1]['name'])
-    #if date in data['schedule'][str(i)]['lessons'][0]['weeks']:
+
+def date_lessons(now_day: int, now_week: int):
+    """Функция определения даты."""
+    day: str = str(now_day)
+    print(data['schedule'][day]['lessons'])
 
 
 if __name__ == '__main__':
-    get_week_lessons(last_week.json()['week'])
+    parse_api(4, 1)
