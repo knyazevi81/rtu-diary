75bdf3a57474a78eea1dccdd31b2cd33fb24d123
Хасан Хамраев <khasanknyazev81@gmail.com>
2023-03-09 10:49:48 +0300

    Update get_timatable.py

--- get_timetable.py
+++ get_timetable.py
@@ -6,6 +6,7 @@
 from requests import get
 import sqlite3
 import datetime
+from readerdata import add_information
 from tokens import groupapi
 from tokens import currentweekapi
 
@@ -19,27 +20,31 @@ con = sqlite3.connect('')
 def calculate_day_by_week(now_day: int, now_week: int) -> datetime:
     """Функция определения даты."""
     start_date: datetime.date = datetime.date(2023, 2, 6)
-    delta: datetime.timedelta = datetime.timedelta(days=7 * (now_week - 1) + now_day - 1)
+    delta: datetime.timedelta = datetime.timedelta(days=7 * (now_week - 1)
+                                                   + now_day - 1)
     return start_date + delta
 
 
-def parse_api(now_day: int, now_week: int):
-    """Персинг пар по типу дня недели."""
+def parse_api_to_data(now_day: int, now_week: int):
+    """Перебор пар по типо дня недели."""
     day: str = str(now_day)
-    print(calculate_day_by_week(now_day, now_week))
     try:
         for i in range(len(data['schedule'][day]['lessons'])):
             for j in range(len(data['schedule'][day]['lessons'][i])):
                 if now_week in data['schedule'][day]['lessons'][i][j]['weeks']:
-                    print(data['schedule'][day]['lessons'][i][j]['name'],
-                          data['schedule'][day]['lessons'][i][j]['types'],
-                          data['schedule'][day]['lessons'][i][j]['rooms'][0],
-                          data['schedule'][day]['lessons'][i][j]['time_start'],
-                          '-',
-                          data['schedule'][day]['lessons'][i][j]['time_end'])
+                    date: str = calculate_day_by_week(now_day, now_week)
+                    lesson: str = data['schedule'][day]['lessons'][i][j]['name']
+                    type_lesson: str = data['schedule'][day]['lessons'][i][j]['types']
+                    rooms: str = data['schedule'][day]['lessons'][i][j]['rooms'][0]
+                    start_time: str = data['schedule'][day]['lessons'][i][j]['time_start']
+                    end_time: str = data['schedule'][day]['lessons'][i][j]['time_end']
+                    add_information(now_week, now_day, lesson, start_time,
+                                    end_time, date, rooms, type_lesson)
     except KeyError:
         print('Выходные')
 
 
 if __name__ == '__main__':
-    parse_api(6, now_week)
+    for now_week in range(1, 3):
+        for now_day in range(1, 7):
+            parse_api_to_data(now_day, now_week)
